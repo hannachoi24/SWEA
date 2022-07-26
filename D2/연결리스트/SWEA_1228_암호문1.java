9a24a66d9a05b10875a8757398beb5ab51210ad8
choihanna <hnc24@naver.com>
2022-07-26 15:09:08 +0900

    SWEA_1228_암호문1

---
+++ D2/연결리스트/SWEA_1228_암호문1.java
@@ -0,0 +1,50 @@
+package D2.연결리스트;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/* SWEA_1228 암호문 리스트로 풀기
+x의 위치에 y개의 숫자를 add해주면 된다. 다만 x의 위치는 add함에 따라 증가시켜준다.
+*/
+
+public class SWEA_1228_암호문1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, k; // n: 원본 암호문의 길이, k: 명령어 개수
+        for (int t = 1; t <= 10; t++) {
+            int count = 0;
+            n = sc.nextInt();
+
+            LinkedList<Integer> list = new LinkedList<>();
+
+            for (int i = 0; i < n; i++) {
+                list.add(sc.nextInt()); // 원본암호문 추가
+            }
+
+            k = sc.nextInt();
+
+            for (int j = 0; j < k; j++) {
+                // String s = sc.next(); // i 제거
+                int x = sc.nextInt(); // 넣을 인덱스
+                int y = sc.nextInt(); // 넣을 숫자 개수
+
+                // 1. I(삽입) x, y, s : 앞에서부터 x의 위치 바로 다음에 y개의 숫자를 삽입한다. s는 덧붙일 숫자들이다.
+                for (int i = 0; i < y; i++) {
+                    list.add(x, sc.nextInt()); // 삽입하면서 x값을 증가시켜 인덱스도 증가
+                    x++;
+                }
+            }
+
+            System.out.printf("#%d ", t);
+            while (!(list.isEmpty())) {
+                if (count > 9)
+                    break;
+                System.out.println(list.poll() + " ");
+                count++;
+
+            }
+            System.out.println();
+        }
+    }
+}
